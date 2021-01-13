d25b3ea70cb3f0467233fd306d4f65f31321902c
thetradecoder <thetradecoder@gmail.com>
2021-01-13 16:36:30 +0600

    testing bootstrap

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
+import Container from 'react-bootstrap/Container';
 export default function About(){
     return(
-        <div>
+        <div className={Container}>
             <h1>Pages created with next js</h1>
             <p>This is working fine!</p>
         </div>
